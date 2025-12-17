fd24d26cb7abff12acce0be93a463d7a171a0ea6
lindexi <lindexi_gd@163.com>
2025-12-17 17:10:21 +0800

    创建新的示例项目

---
+++ src/DotNetCampus.Installer.AvaloniaSample/MainWindow.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace DotNetCampus.Installer.AvaloniaSample;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
