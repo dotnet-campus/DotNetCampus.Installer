d76423c9b00d9fb4b6d5bfa4ff21f9c1f6ea12fc
lindexi <lindexi_gd@163.com>
2025-12-17 16:02:21 +0800

    加上注释内容

--- src/DotNetCampus.Installer.Lib/InstallerHost.cs
+++ src/DotNetCampus.Installer.Lib/InstallerHost.cs
@@ -25,6 +25,7 @@ namespace DotNetCampus.Installer.Lib;
 /// <summary>
 /// 安装器主机
 /// </summary>
+/// 此对象可以继承和重写里面的很多方法来实现自定义的安装器主机行为。默认情况下，都是将独立的安装器打包作为资源，启动的时候解压缩出来运行。默认的情况的方法会导致安装包启动速度比较慢，毕竟需要解压缩和被杀毒扫描。自定义安装器可以直接立刻运行，但要求 UI 界面安装程序能够支持 AOT 构建
 public class InstallerHost
 {
     public static InstallerHostBuilder CreateBuilder()
