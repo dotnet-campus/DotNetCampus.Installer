9898e08a6bdb9d5a9041747ef1079d92e3ab025d
lindexi <lindexi_gd@163.com>
2025-12-17 16:26:55 +0800

    允许重写不执行检测环境

--- src/DotNetCampus.Installer.Lib/Hosts/Contexts/InstallContext.cs
+++ src/DotNetCampus.Installer.Lib/Hosts/Contexts/InstallContext.cs
@@ -1,4 +1,6 @@
-﻿namespace DotNetCampus.Installer.Lib.Hosts.Contexts;
+﻿using DotNetCampus.Installer.Lib.SplashScreens;
+
+namespace DotNetCampus.Installer.Lib.Hosts.Contexts;
 
 /// <summary>
 /// 安装过程的上下文信息
@@ -7,5 +9,14 @@
 /// <param name="SplashScreenWindowHandler">欢迎界面的句柄</param>
 public record InstallContext(InstallerHostConfiguration Configuration, IntPtr SplashScreenWindowHandler)
 {
-
+    /// <summary>
+    /// 关闭欢迎界面
+    /// </summary>
+    public void CloseSplashScreenWindow()
+    {
+        if (SplashScreenWindowHandler != 0)
+        {
+            SplashScreen.CloseSplashScreenWindow(SplashScreenWindowHandler);
+        }
+    }
 }
--- src/DotNetCampus.Installer.Lib/Hosts/Contexts/InstallerHostConfiguration.cs
+++ src/DotNetCampus.Installer.Lib/Hosts/Contexts/InstallerHostConfiguration.cs
@@ -18,6 +18,7 @@ public readonly record struct InstallerHostConfiguration
     /// <summary>
     /// 里层带界面的安装器所在的嵌入程序集的压缩包资源
     /// </summary>
+    /// 可以没有安装器，表示不需要运行里层安装器，可以换成自定义的安装器逻辑
     public required AssemblyManifestResourceInfo? InstallerResourceAssetsInfo { get; init; }
 
     /// <summary>
@@ -29,4 +30,6 @@ public readonly record struct InstallerHostConfiguration
     /// 启动里层带界面的安装器时的配置
     /// </summary>
     public Action<ProcessStartInfoConfigurationContext>? InstallerProcessStartConfigAction { get; init; }
+
+
 }
