148c5a0df4c1c5ac5df811c0eff77cab37671894
lindexi <lindexi_gd@163.com>
2025-12-23 20:31:25 +0800

    加上图标设置

--- src/DotNetCampus.Installer.Lib/Utils/ShortcutHelper.cs
+++ src/DotNetCampus.Installer.Lib/Utils/ShortcutHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.Versioning;
 
 using Windows.Win32.System.Com;
+using Windows.Win32.UI.Shell;
 
 namespace DotNetCampus.Installer.Lib.Utils;
 
@@ -9,17 +10,21 @@ public static class ShortcutHelper
     /// <summary>
     /// 创建一个快捷方式
     /// </summary>
-    /// <param name="lnkFilePath">快捷方式的完全限定路径。</param>
+    /// <param name="lnkFilePath">快捷方式的完全限定路径。也可以是 URL 网址</param>
     /// <param name="workDir"></param>
     /// <param name="args">快捷方式启动程序时需要使用的参数。</param>
-    /// <param name="targetPath">快捷方式指向的目标路径。</param>
+    /// <param name="targetPath"></param>
+    /// <param name="iconFile"></param>
     [SupportedOSPlatform("windows5.1.2600")]
-    public static unsafe void CreateShortcut(string lnkFilePath, string targetPath, string workDir, string args = "")
+    public static unsafe void CreateShortcut(string lnkFilePath, string targetPath, string workDir, string args = "", string iconFile = "")
     {
-        var shellLinkW = ShellLinkProvider.CreateShellLink();
+        IShellLinkW* shellLinkW = ShellLinkProvider.CreateShellLink();
+
         shellLinkW->SetPath(targetPath);
         shellLinkW->SetArguments(args);
         shellLinkW->SetWorkingDirectory(workDir);
+
+        shellLinkW->SetIconLocation(iconFile, -1);
 
         shellLinkW->QueryInterface(out IPersistFile* persistFile);
         //persistFile->SaveCompleted(lnkFilePath);
