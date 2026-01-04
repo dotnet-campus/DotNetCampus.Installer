1b7f85a7b3b84cbe4566a8e2fb6428a3a21003fd
lindexi <lindexi_gd@163.com>
2026-01-04 14:37:13 +0800

    测试读取千万次

--- src/InstallerSevenZipLib/DirectoryArchives/SliceStream.cs
+++ src/InstallerSevenZipLib/DirectoryArchives/SliceStream.cs
@@ -36,7 +36,7 @@ public class SliceStream : Stream
         buffer = buffer.Slice(0, toRead);
 
         var read = _originStream.Read(buffer);
-        Position += read;
+        _position += read;
         return read;
     }
 
@@ -47,7 +47,7 @@ public class SliceStream : Stream
         var toRead = (int) Math.Min(count, Length - Position);
 
         var read = await _originStream.ReadAsync(buffer, offset, toRead, cancellationToken);
-        Position += read;
+        _position += read;
         return read;
     }
 
@@ -59,20 +59,24 @@ public class SliceStream : Stream
 
         var read = await _originStream.ReadAsync(buffer.Slice(0, toRead), cancellationToken);
 
-        Position += read;
+        _position += read;
         return read;
     }
 
     public override int Read(byte[] buffer, int offset, int count)
     {
+        _readCount++;
+
         UpdatePositionForOriginStream();
 
         var toRead = (int) Math.Min(count, Length - Position);
 
         var read = _originStream.Read(buffer, offset, toRead);
-        Position += read;
+        _position += read;
         return read;
     }
+
+    private int _readCount;
 
     private void UpdatePositionForOriginStream()
     {
@@ -85,7 +89,7 @@ public class SliceStream : Stream
 
     public override long Seek(long offset, SeekOrigin origin)
     {
-        Position = origin switch
+        _position = origin switch
         {
             SeekOrigin.Begin => offset,
             SeekOrigin.Current => Position + offset,
@@ -109,7 +113,13 @@ public class SliceStream : Stream
     public override bool CanSeek => true;
     public override bool CanWrite => false;
     public override long Length { get; }
-    public override long Position { get; set; }
+
+    public override long Position
+    {
+        get => _position;
+        set => _position = value;
+    }
+    private long _position;
 
     protected override void Dispose(bool disposing)
     {
