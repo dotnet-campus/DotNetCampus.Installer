b432384f794f1ec95140d66515a998b68fab2f56
lindexi <lindexi_gd@163.com>
2026-01-04 14:15:14 +0800

    合并为一个类

---
+++ src/InstallerSevenZipLib/DirectoryArchives/DirectoryArchiveFileInfo.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.DotNet.Archive;
+
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCampus.InstallerSevenZipLib.DirectoryArchives;
+
+public readonly record struct DirectoryArchiveFileInfo(string RelativePath, FileInfo FileInfo)
+{
+}
